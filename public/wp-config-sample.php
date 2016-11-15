6111b492972f395b19784a69fe0ac4503165113a
Théo Kleman <theo@kleman.fr>
2016-11-15 22:00:47 +0100

    Add nav items target blank and enable WP debug

--- public/wp-config-sample.php
+++ public/wp-config-sample.php
@@ -82,7 +82,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* C’est tout, ne touchez pas à ce qui suit ! */
 
